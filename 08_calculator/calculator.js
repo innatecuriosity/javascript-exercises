c38a6db3cb5299583f715b509131b06f44d3b6b3
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 16:45:26 +0100

    08 ez

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(...args) {
+	return sum(args);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let sum = 0;
+  for (const num of list) { sum += num}
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(list) {
+  mult = 1;
+  for (num of list) {mult *= num}
 
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let fact = 1;
+	for (let i=1; i<=a; i++ ){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
