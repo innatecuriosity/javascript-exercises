28716bf3c5dc5bc3e9c7214bc8e2e96e4a488728
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 17:09:50 +0100

    10 solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
 
+    if (n<0) {return "OOPS"}
+
+    let previous = 0;
+    let current =1;
+    for (i = 1; i<n; i++) {
+        temp = previous + current;
+        previous = current;
+        current = temp;
+    }
+    return current;
 };
 
 // Do not edit below this line
