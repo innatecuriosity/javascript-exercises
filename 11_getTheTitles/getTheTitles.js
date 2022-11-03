0be65bef21e4c6c1899d464666ef9a6bf2d4cbd6
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 17:13:41 +0100

    11, yay attributes work the same as python

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    titles=[];
+    for (dict of list) {titles.push(dict.title)}
+    return titles;
 };
 
 // Do not edit below this line
