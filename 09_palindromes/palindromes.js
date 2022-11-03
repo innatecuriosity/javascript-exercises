dea1e7215e31ea440c3e6c71da3923890dba48b9
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 17:01:25 +0100

    09 solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // remove punctuation and spaces
+    word = word.toLowerCase();
+    let clean = "";
+    for (const sign of word){
+        for (const letter of "abcdefghijklmnoprqstuvxyz") {
+            if (sign==letter)  {clean += sign; break;}
+            continue;
+        }
+    }
+    console.log(clean);
+    for (let i=0; i<(clean.length/2); i++) {
+        if (clean[i] != clean[clean.length - 1 - i]) {return false;}
+    }
+    return true;
 };
+
+palindromes("This is confuzing.");
 
 // Do not edit below this line
 module.exports = palindromes;
