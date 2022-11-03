50735c38f9fe9bc527cffd6f13f33037b9859534
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 16:14:05 +0100

    04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(list, ...args) {
+    for (const arg of args) {
+        for (let i= 0; i<list.length; i++){
+            if (list[i]===arg) {
+                list.splice(i,1);
+            }
+        }
+    }
+    return list;
+    
+}
 // Do not edit below this line
 module.exports = removeFromArray;
