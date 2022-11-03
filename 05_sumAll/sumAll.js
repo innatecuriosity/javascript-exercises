12a430fa7afeb58b86f432e6192ca583bebdb1ea
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-03 16:27:46 +0100

    05 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof num1 != 'number') || (typeof num2 != 'number')) {return "ERROR"}
+
+    let lowest = num1;
+    let highest = num2;
+
+    if (num1>num2) {lowest = num2; highest = num1;}
+
+    if (lowest <0) {return "ERROR"}
+
+    let sum = 0;
+
+    for (let i = lowest; i <= highest; i++) {sum += i}
+
+    return sum;
+
 
 };
 
