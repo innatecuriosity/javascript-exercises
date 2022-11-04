787d3be8e5a07adbb618779fe967a4245c8178ec
innatecuriosity <116571759+innatecuriosity@users.noreply.github.com>
2022-11-04 09:52:18 +0100

    12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = 2022;
+    let oldest;
+    let oldestAge=0;
 
+    for (person of people){
+        let yearOfDeath = person["yearOfDeath"]; 
+        if (yearOfDeath==undefined) {yearOfDeath = currentYear}
+
+        age = yearOfDeath - person["yearOfBirth"];
+        
+        if (age>oldestAge) {
+            oldestAge = age;
+            oldest = person;
+            console.log([age, oldestAge, person])
+        }
+    }
+    return oldest;
+        
 };
 
 // Do not edit below this line
